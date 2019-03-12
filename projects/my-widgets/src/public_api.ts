9c735f12f2138d5ef5e0c25baa3e00a99cebc80f
Ivan Pranovich <ivan.pranovich@gmail.com>
2019-03-12 19:00:40 +0300

    Removed default widget

--- projects/my-widgets/src/public_api.ts
+++ projects/my-widgets/src/public_api.ts
@@ -2,7 +2,4 @@
  * Public API Surface of my-widgets
  */
 
-export * from './lib/my-widgets.service';
-export * from './lib/my-widgets.component';
-export * from './lib/my-widgets.module';
 export * from './lib/table/table.module';
