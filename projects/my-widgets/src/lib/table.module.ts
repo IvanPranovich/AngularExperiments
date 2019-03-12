7ad0728b1ed1ff3fe8a00b4f690a24adc643988a
Ivan Pranovich <ivan.pranovich@gmail.com>
2019-03-12 19:10:27 +0300

    Added templates tfor able-cell and table-row

--- projects/my-widgets/src/lib/table.module.ts
+++ projects/my-widgets/src/lib/table.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TableComponent } from './table/table.component';
+import { TableRowComponent } from './table-row/table-row.component';
+import { TableCellComponent } from './table-cell/table-cell.component';
 
 @NgModule({
-  declarations: [TableComponent],
+  declarations: [TableComponent, TableRowComponent, TableCellComponent],
   imports: [
     CommonModule
   ]
