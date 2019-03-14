2bf0d95f726e1981e623a755a8d95b69b990ca0e
Ivan Pranovich <ivan.pranovich@gmail.com>
2019-03-14 21:41:19 +0300

    Added common cell component

---
+++ projects/my-widgets/src/lib/common/my-table-cell-component.ts
@@ -0,0 +1,25 @@
+import { Input, Output, EventEmitter, OnInit, OnChanges } from '@angular/core';
+
+export class MyTableCellComponent implements OnChanges {
+    set currentValue(newValue: any) {
+        this._currentValue = newValue;
+        this.valueChanged();
+    }
+    get currentValue(): any {
+        return this._currentValue;
+    }
+
+    @Input() initialValue: any;
+    @Output() valueChange = new EventEmitter();
+
+    private _currentValue: any;
+
+    ngOnChanges(): void {
+        // Dirty fix for initial value
+        this._currentValue = this.initialValue;
+    }
+
+    valueChanged() {
+        this.valueChange.emit(this._currentValue);
+    }
+}
--- projects/my-widgets/src/lib/table.module.ts
+++ projects/my-widgets/src/lib/table.module.ts
@@ -3,11 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MyTableComponent } from './my-table/my-table.component';
 import { MyTableRowComponent } from './my-table-row/my-table-row.component';
 import { MyTableCellComponent } from './my-table-cell/my-table-cell.component';
+import { MyCheckboxComponent } from './my-checkbox/my-checkbox.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [MyTableComponent, MyTableRowComponent, MyTableCellComponent],
+  declarations: [MyTableComponent, MyTableRowComponent, MyTableCellComponent, MyCheckboxComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [MyTableComponent, MyTableRowComponent, MyTableCellComponent]
 })
